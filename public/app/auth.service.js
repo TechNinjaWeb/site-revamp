f24d956523485a7caa85c5d4e8ea3f99471de7c9
Rahim <info@tech-ninja.ca>
2018-08-01 02:19:00 -0400

    minor cleanup in preparation comoponent building in Angular 2+

--- public/app/auth.service.js
+++ public/app/auth.service.js
@@ -1,5 +1,5 @@
-angular.module('auth.service', ['backand'])
-    .service('AuthService', ['Backand', function AuthService(Backand) {
+angular.module('auth.service', [])
+    .service('AuthService', [function AuthService() {
 
         var self = this;
         self.currentUser = {};
@@ -7,76 +7,85 @@ angular.module('auth.service', ['backand'])
         loadUserDetails();
 
         function loadUserDetails() {
-
-            return Backand.getUserDetails()
-                .then(function(data) {
-                    self.currentUser.details = data;
-                    if (data !== null)
-                        self.currentUser.name = data.username;
-                });
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.getUserDetails()
+            //     .then(function(data) {
+            //         self.currentUser.details = data;
+            //         if (data !== null)
+            //             self.currentUser.name = data.username;
+            //     });
 
         }
 
         self.getSocialProviders = function() {
-            return Backand.getSocialProviders()
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.getSocialProviders()
         };
 
         self.socialSignin = function(provider) {
-            Backand.setRunSignupAfterErrorInSigninSocial(false); //by default run sign-up if there is no sign in
-            return Backand.socialSignin(provider)
-                .then(function(response) {
-                    loadUserDetails();
-                    return response;
-                });
+            return console.log("Backand was replaced and needs rebuilding");
+            // Backand.setRunSignupAfterErrorInSigninSocial(false); //by default run sign-up if there is no sign in
+            // return Backand.socialSignin(provider)
+            //     .then(function(response) {
+            //         loadUserDetails();
+            //         return response;
+            //     });
         };
 
         self.socialSignup = function(provider) {
-            return Backand.socialSignUp(provider)
-                .then(function(response) {
-                    loadUserDetails();
-                    return response;
-                });
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.socialSignUp(provider)
+            //     .then(function(response) {
+            //         loadUserDetails();
+            //         return response;
+            //     });
         };
 
         self.signin = function(username, password) {
-            return Backand.signin(username, password)
-                .then(function(response) {
-                    loadUserDetails();
-                    return response;
-                });
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.signin(username, password)
+            //     .then(function(response) {
+            //         loadUserDetails();
+            //         return response;
+            //     });
         };
 
         self.signup = function(firstName, lastName, username, password, parameters) {
-            return Backand.signup(firstName, lastName, username, password, password, parameters)
-                .then(function(signUpResponse) {
-                    if (signUpResponse.data.currentStatus === 1) {
-                        return self.signin(username, password)
-                            .then(function() {
-                                return signUpResponse;
-                            });
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.signup(firstName, lastName, username, password, password, parameters)
+            //     .then(function(signUpResponse) {
+            //         if (signUpResponse.data.currentStatus === 1) {
+            //             return self.signin(username, password)
+            //                 .then(function() {
+            //                     return signUpResponse;
+            //                 });
 
-                    } else {
-                        return signUpResponse;
-                    }
-                });
+            //         } else {
+            //             return signUpResponse;
+            //         }
+            //     });
         };
 
         self.changePassword = function(oldPassword, newPassword) {
-            return Backand.changePassword(oldPassword, newPassword)
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.changePassword(oldPassword, newPassword)
         };
 
         self.requestResetPassword = function(username) {
-            return Backand.requestResetPassword(username)
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.requestResetPassword(username)
         };
 
         self.resetPassword = function(password, token) {
-            return Backand.resetPassword(password, token)
+            return console.log("Backand was replaced and needs rebuilding");
+            // return Backand.resetPassword(password, token)
         };
 
         self.logout = function() {
-            Backand.signout().then(function() {
-                angular.copy({}, self.currentUser);
-            });
+            return console.log("Backand was replaced and needs rebuilding");
+            // Backand.signout().then(function() {
+            //     angular.copy({}, self.currentUser);
+            // });
         };
 
     }]);
